af1a17844dc6e717fd513e8e4e72f328054b8469
w4d32001 <freedom01022001@gmail.com>
2026-01-19 10:00:37 -0500

    forzar estatico

--- app/api/auth/forgot-password/route.ts
+++ app/api/auth/forgot-password/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client'
 import crypto from 'crypto'
 import nodemailer from 'nodemailer'
-
+export const dynamic = 'force-dynamic';
 const prisma = new PrismaClient()
 
 export async function POST(request: NextRequest) {
